a8fc3e2ba53f28e3c729651f65bac4e4511549c6
shukla06 <shukla06@students.cs.ubc.ca>
2025-01-08 12:49:57 -0800

    finish function to calculate angle between points

--- Exercises.py
+++ Exercises.py
@@ -1,20 +1,49 @@
-
+import math
 
 class Exercise:
 
     #contains all general functionality used to analyze an exercise
     
-    def __init__(self):
-        pass
+    def __init__(self, landmarks):
+        self.landmarks = landmarks
 
 
-    def calculate_angle(one, two , three):
-        pass
-        #calculate the angle between the three points using two as the midpoint
+    #(a,b,c) are points contained in dictionaries with keys "x" and "y", z is disregarded
+    def calculate_angle(a,b,c):
+        
+        BA = [a['x'] - b['x'], a['y'] - b['y']]
+        BC = [c['x'] - b['x'], c['y'] - b['y']]
 
+        dot_prod = (BA[0] * BC[0]) + (BA[1] * BC[1])
 
+        mag_BA = math.sqrt((BA[0] ** 2) + (BA[1] ** 2))
+        mag_BC = math.sqrt((BC[0] ** 2) + (BC[1] ** 2))
 
+        cos_angle = dot_prod/ (mag_BA * mag_BC)
+        cos_angle = max(min(cos_angle, 1.0), -1.0)
+
+        angle = math.degrees(math.acos(cos_angle))
+        return angle
 
 
 class Squat(Exercise):
-   pass 
+
+
+   #returns true or false depending if shoulders are over knees
+   def check_shoulders_past_knees():
+       pass
+   
+   #check if heels are raised
+   def check_raised_heels():
+       pass
+   
+
+   #check if hip and knee angles are algined
+   def check_hip_knee_angles():
+       pass
+
+
+
+       
+
+
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import cv2
 import mediapipe as mp
+from Exercises import Squat
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
-image = cv2.imread('./image.png')
+image = cv2.imread('./reasources/image.png')
 
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -15,6 +16,12 @@ with mp_holistic.Holistic(
     results = holistic.process(image)
     
 
+    if results.pose_landmarks:
+        for idx, landmark in enumerate(results.pose_landmarks.landmark):
+            print(f"Landmark {idx}: x={landmark.x}, y={landmark.y}, z={landmark.z}, visibility={landmark.visibility}")
+
+    sq = Squat(results.pose_landmarks)
+
 
 
     mp_drawing.draw_landmarks(
@@ -23,8 +30,16 @@ with mp_holistic.Holistic(
         mp_holistic.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles
         .get_default_pose_landmarks_style())
+    
+    # Overlay text on the image
+    text = "this is how you add text"
+    position = (50, 50)  # (x, y) coordinates of the text
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    color = (0, 255, 0)  # Green color in BGR
+    thickness = 2
+    cv2.putText(image, text, position, font, font_scale, color, thickness, cv2.LINE_AA)
  # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
+    cv2.imshow('MediaPipe Holistic', image)
 cv2.waitKey(0)
-#print(results)
 
